4c9b41d6d92476d27c511a3bdae05da39db919ef
seoweed <darkweed1106@gmail.com>
2024-08-27 21:11:24 +0900

    Initial commit project

---
+++ src/main/java/com/mtvs/mtvs3rdbe/Mtvs3rdBeApplication.java
@@ -0,0 +1,13 @@
+package com.mtvs.mtvs3rdbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mtvs3rdBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mtvs3rdBeApplication.class, args);
+	}
+
+}
