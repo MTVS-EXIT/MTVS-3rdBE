4ece9a65d1db40a4a5c5ebfcb494f1d280eaf5f3
seoweed <darkweed1106@gmail.com>
2024-08-27 21:11:24 +0900

    Initial commit project

---
+++ src/test/java/com/mtvs/mtvs3rdbe/Mtvs3rdBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtvs.mtvs3rdbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mtvs3rdBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
